50acb92d1291e1a9bd2e97af8a82e5f75be80c3d
KaidenWilliams <kman16409@gmail.com>
2024-12-11 01:01:57 -0700

    updated so it batches

--- tweeter-server/src/model/service/QueueService.ts
+++ tweeter-server/src/model/service/QueueService.ts
@@ -1,4 +1,4 @@
-import { SQSClient, SendMessageCommand } from "@aws-sdk/client-sqs";
+import { SQSClient, SendMessageBatchCommand, SendMessageCommand } from "@aws-sdk/client-sqs";
 import { StatusDto } from "tweeter-shared";
 import { config } from "../../config/config";
 import { FeedEntity } from "../entity/FeedEntity";
@@ -7,6 +7,7 @@ export class QueueService {
   private sqsClient: SQSClient;
   private POST_STATUS_QUEUE_URL = "https://sqs.us-east-2.amazonaws.com/214630358865/PostStatusQueue";
   private POST_FEED_QUEUE_URL = "https://sqs.us-east-2.amazonaws.com/214630358865/PostFeedQueue";
+  BATCH_SIZE = 10;
 
   constructor() {
     this.sqsClient = new SQSClient({ region: config.AWS_REGION });
@@ -30,15 +31,20 @@ export class QueueService {
   // SENDS ALL FEEDENTITIES TO QUEUE
   async sendFeedMessagesToQueue(feedMessages: FeedEntity[]): Promise<void> {
     try {
-      const sendPromises = feedMessages.map((message) => {
-        const command = new SendMessageCommand({
-          QueueUrl: this.POST_FEED_QUEUE_URL,
-          MessageBody: JSON.stringify(message),
-        });
-        return this.sqsClient.send(command);
-      });
+      for (let i = 0; i < feedMessages.length; i += this.BATCH_SIZE) {
+        const batch = feedMessages.slice(i, i + this.BATCH_SIZE);
 
-      await Promise.all(sendPromises);
+        const command = new SendMessageBatchCommand({
+          // Unique ID for each message in the batch needed
+          QueueUrl: this.POST_FEED_QUEUE_URL,
+          Entries: batch.map((message, index) => ({
+            Id: `msg-${i + index}`,
+            MessageBody: JSON.stringify(message),
+          })),
+        });
+
+        await this.sqsClient.send(command);
+      }
     } catch (error) {
       console.error("Error sending feed messages to queue:", error);
       throw new Error("Failed to send feed messages");
